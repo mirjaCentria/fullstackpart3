fbfb6602b164b52d403b26a7f27b27b583b3a8a7
Mirja Uusitalo <mirja.uusitalo@fougait.com>
2020-11-12 01:09:47 +0200

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
     "name": "Arto Hellas",
@@ -23,7 +25,6 @@ let persons = [
     "id": 4
     }
 ]
-
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
@@ -39,7 +40,6 @@ app.get('/api/persons/:id', (request, response) => {
   console.log(id)
   const person = persons.find(person => person.id === id)
   console.log(person)
-
   if (person) {
       response.json(person)
   } else {
@@ -54,6 +54,24 @@ app.delete('/api/persons/:id', (request, response) => {
   console.log(persons)
   response.status(204).end()
 })
+
+app.post('/api/persons', (request, response) => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id)) 
+    : 0
+  const person = request.body
+  person.id = maxId + 1
+  persons = persons.concat(person)
+  console.log(person)
+  response.json(person)
+})
+
+
+
+
+
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
