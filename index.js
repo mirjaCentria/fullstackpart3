fb58822bedf60f1924b7c6716e412ede43aadd0d
Mirja Uusitalo <mirja.uusitalo@fougait.com>
2020-11-11 23:54:29 +0200

    3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  persons = persons.filter(person => person.id !== id)
+  console.log(persons)
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
