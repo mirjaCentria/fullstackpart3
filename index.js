a5a5cd8c86edcf6197c02dcab88e930eb3d63247
Mirja Uusitalo <mirja.uusitalo@fougait.com>
2020-11-17 23:59:15 +0200

    3.8: Phonebook backend step8 not ok

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const morgan = require('morgan')
 
 
 app.use(express.json())
-app.use(morgan('tiny'))
+//app.use(morgan('tiny'))
+
+morgan.token('body', (req, res) => {
+    
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body)
+    } else {
+        return null
+    }
+})
+app.use(morgan(':method :url :status :response[content-length] - :response-time ms :body'))
 
 let persons = [
     {
