3b1c7b5f22294a0e6e6ac58394dc0a3ee01153a5
Mirja Uusitalo <mirja.uusitalo@fougait.com>
2020-11-20 23:52:33 +0200

    3.11 3.12ish

--- build/mongo.js
+++ build/mongo.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-
-if (process.argv.length < 3) {
+if(pocess.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
@@ -10,23 +9,29 @@ const password = process.argv[2]
 const url =
   `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, 
+  useFindAndModify: false, useCreateIndex: true })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
+/*
+const person = new Person({
+  name: 'Arto Vihavainen',
+  number: '045-1232456',
+})*/
 
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
+Person.find({}).then(result => {
+  result.forEach(person => {
+    console.log(person)
+  })
+  mongoose.connection.close()
 })
 
-note.save().then(result => {
-  console.log('note saved!')
+person.save().then(result => {
+  console.log('person saved!')
   mongoose.connection.close()
 })
