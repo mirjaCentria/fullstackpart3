723e2de3c831180d4ecfeee11818245b074489ad
Mirja Uusitalo SOFTK20P <mirja.uusitalo@centria.fi>
2020-12-02 14:01:33 +0200

    3.15, 3.16 ok

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const Person = require('./models/person')
 
 const app = express()
 app.use(express.static('build'))
-
 app.use(express.json())
-
 app.use(cors())
 
 //app.use(morgan('tiny'))
@@ -40,36 +38,36 @@ app.get('/info', cors(), (request, response) => {
   response.send(`<p>${info}</p><p>${date}</p>`)
 })
 
-app.get('/api/persons/:id', cors(), (request, response) => {
+app.get('/api/persons/:id', cors(), (request, response, next) => {
   const id = request.params.id
   console.log(id)
-  const person = persons.find(person => person.id === id)
-  console.log(person)
-  if (person) {
-      response.json(person)
-  } else {
-      response.status(404).end()
-  }
+  Person.findById(request.params.id)
+  .then(person => {
+   console.log(person)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', cors(), (request, response) => {
   Person.findByIdAndRemove(request.params.id)
   .then(result => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
     response.status(204).end()
   })
   .catch(error => next(error))
-  const id = request.params.id
-  console.log(id)
-  persons = persons.filter(person => person.id !== id)
-  console.log(persons)
-  response.status(204).end()
 })
 
 
 app.post('/api/persons', cors(), (request, response) => {
   const body = request.body
-  console.log('apppost', body.name)
-  /*
+ // console.log('apppost', body.name)
+  
   if (body.name == null || body.number == null ) {
     return response.status(400).json({
         error: 'number is missing'
@@ -90,23 +88,38 @@ app.post('/api/persons', cors(), (request, response) => {
     return response.status(400).json({ 
       error: 'number is missing!' 
     })
-  }*/
+  }
 
-  console.log('apppost2')
-const person = new Person({
-  name: body.name,
-  number: body.number
-})
-console.log('apppost3')
-person.save().then(saved => {
-  response.json(saved)
-})
-
-console.log('apppost4')
+ // console.log('apppost2')
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+ // console.log('apppost3')
+  person.save().then(saved => {
+    response.json(saved)
+  })
   persons = persons.concat(person)
-  console.log('app.post ')
- // response.json(person) 
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 
 //const PORT = process.env.PORT 
 const PORT = 3001
